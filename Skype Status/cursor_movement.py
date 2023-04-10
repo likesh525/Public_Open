c8ae0592b5393777d97162aba8ea69129f7869e3
likesh525 <102144072+likesh525@users.noreply.github.com>
2023-04-10 15:02:51 +0530

    Update cursor_movement.py

--- Skype Status/cursor_movement.py
+++ Skype Status/cursor_movement.py
@@ -11,15 +11,24 @@ while True:
     x, y = screen.size()
     x1 = random.randint(0, x)
     y1 = random.randint(0, y)
+    
+    # If the generated random number is greater than the screen size, it will throw up an error and interrupt the infinite loop.
+    
     if x1 > x:
         x1 = x
     if y1 > y:
         y1 = y
+        
     screen.moveTo(x1, y1)
-    # Select a location to move cursor, i choose 700,300 cuz the location was blacnk with no options selections,
-    # also, click is required to keep skpyee status online
+    
+    # I choose 700,300 because the location was blank. 
+    # Additionally, clicking is required to keep SKPYE's status online. 
+    
+    
     screen.click(700, 300, button='left')
+    # Repeat the same every 60 seconds
     time.sleep(60)
+    
 
 
 
